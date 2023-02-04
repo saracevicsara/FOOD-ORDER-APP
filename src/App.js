2f6063c5a96765a3530529c3cc2d21177aa13bab
unknown <sarasaravevic@gmail.com>
2023-02-04 17:47:53 +0100

    adding state to cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Cart from "./Components/Cart/Cart";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = (props) => {
+    setCartIsShown(true);
+  };
+  const hideCartHandler = (props) => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart />}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>React meals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt={"a table with the food"} />
